cd6a118e5022da54ef906e387f9a4c0b50b675e7
vladsozonov01 <vladsozonov01@gmail.com>
2025-09-04 15:04:04 +0000

    fix: add @types/xlsx to dependencies

--- dist/handlers/errorHandler.js
+++
@@ -1,9 +0,0 @@
-class ErrorHandler {
-    handleBotError(error, ctx) {
-        console.error("❌ Ошибка бота:", error);
-        if (ctx && ctx.reply) {
-            ctx.reply("⚠️ Произошла непредвиденная ошибка. Попробуйте позже или обратитесь к администратору.");
-        }
-    }
-}
-export default new ErrorHandler();
